14b7a395f110da5c9a058221547cd838d7813764
JihenAridhi <aridhijihen1@gmail.com>
2025-06-12 04:55:54 +0100

    wallet page

--- src/app/components/security/admin-dashboard/wallet-mng/wallet-mng.component.ts
+++ src/app/components/security/admin-dashboard/wallet-mng/wallet-mng.component.ts
@@ -9,39 +9,74 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class WalletMngComponent {
 
   ngOnInit(): void {
-    this.showTab('basic-info'); // or any other tabId you want as default
   }
 
   isWalletFormVisible: boolean = false;
   isWalletDetailsVisible: boolean = false;
-  isAdvancedFilterVisible: boolean = false;
+  isWalletStatusVisible: boolean = false;
+  isWalletTypeVisible: boolean = false;
+  isWalletCategoryVisible: boolean = false;
+  isCardFormVisible: boolean = false;
+  isCardTypeVisible: boolean = false;
+  isCardListVisible: boolean = false;
+  isAccountFormVisible: boolean = false;
+  isAccountTypeVisible: boolean = false;
+  isAccountListVisible: boolean = false;
 
-  // Toggle the visibility of the wallet creation form
-  toggleWalletForm(): void {
-    this.isWalletFormVisible = true;
-  }
-  // Optional: Method to close the form (e.g., when clicking "Cancel")
-  closeWalletForm(): void {
-    this.isWalletFormVisible = false;
-  }
-
-  // Toggle the visibility of the wallet creation form
-  toggleWalletDetails(): void {
-    this.isWalletDetailsVisible = true;
-  }
-  // Optional: Method to close the form (e.g., when clicking "Cancel")
-  closeWalletDetails(): void {
-    this.isWalletDetailsVisible = false;
+  toggleForm(modal: String): void {
+    switch (modal) {
+      case 'create-wallet': this.isWalletFormVisible = true; break;
+      case 'wallet-details': this.isWalletDetailsVisible = true; break;
+      case 'wallet-status': this.isWalletStatusVisible = true; break;
+      case 'wallet-type': this.isWalletTypeVisible = true; break;
+      case 'wallet-category': this.isWalletCategoryVisible = true; break;
+      case 'create-card': this.isCardFormVisible = true; break;
+      case 'card-type': this.isCardTypeVisible = true; break;
+      case 'card-list': this.isCardListVisible = true; break;
+      case 'create-account': this.isAccountFormVisible = true; break;
+      case 'account-type': this.isAccountTypeVisible = true; break;
+      case 'account-list': this.isAccountListVisible = true; break;
+    }
   }
 
-  // Toggle the advanced filter
-  toggleAdvancedFilter(): void {
-    this.isAdvancedFilterVisible = true;
+  closeForm(modal: String): void {
+    switch (modal) {
+      case 'create-wallet': this.isWalletFormVisible = false; break;
+      case 'wallet-details': this.isWalletDetailsVisible = false; break;
+      case 'wallet-status': this.isWalletStatusVisible = false; break;
+      case 'wallet-type': this.isWalletTypeVisible = false; break;
+      case 'wallet-category': this.isWalletCategoryVisible = false; break;
+      case 'create-card': this.isCardFormVisible = false; break;
+      case 'card-type': this.isCardTypeVisible = false; break;
+      case 'card-list': this.isCardListVisible = false; break;
+      case 'create-account': this.isAccountFormVisible = false; break;
+      case 'account-type': this.isAccountTypeVisible = false; break;
+      case 'account-list': this.isAccountListVisible = false; break;
+    }
   }
 
-  showTab(tabId: string): void {
-    const tabButtons = document.querySelectorAll('.tab-button');
-    const tabContents = document.querySelectorAll('.tab-content');
+  get isAnyModalVisible(): boolean {
+    return (
+      this.isWalletDetailsVisible ||
+      this.isWalletFormVisible ||
+      this.isWalletStatusVisible ||
+      this.isWalletTypeVisible ||
+      this.isWalletCategoryVisible ||
+      this.isCardFormVisible ||
+      this.isCardTypeVisible ||
+      this.isCardListVisible ||
+      this.isAccountFormVisible ||
+      this.isAccountTypeVisible ||
+      this.isAccountListVisible
+    );
+  }
+
+  showTab(tabId: string, tabType?: string): void {
+
+    const buttonClass = tabType ? `${tabType}-tab-button` : 'tab-button';
+    const contentClass = tabType ? `${tabType}-tab-content` : 'tab-content';
+    const tabButtons = document.querySelectorAll(`.${buttonClass}`);
+    const tabContents = document.querySelectorAll(`.${contentClass}`);
 
     // Reset all buttons and contents
     tabButtons.forEach(btn => {
@@ -56,7 +91,8 @@ export class WalletMngComponent {
     activeButton?.classList.add('active', 'text-primary', 'font-medium');
     activeButton?.classList.remove('text-gray-500');
 
-    const activeContent = document.getElementById(`tab-${tabId}`);
+    const activeId = tabType ? `${tabType}-tab-${tabId}` : `tab-${tabId}`;
+    const activeContent = document.getElementById(activeId);
     activeContent?.classList.remove('hidden');
   }
 
