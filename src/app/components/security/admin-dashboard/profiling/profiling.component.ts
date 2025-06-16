ef5988b80e6e7d92a32d1a5b5f6f7beae175cfb1
JihenAridhi <aridhijihen1@gmail.com>
2025-06-16 23:04:05 +0100

    iden

---
+++ src/app/components/security/admin-dashboard/profiling/profiling.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profiling',
+  imports: [],
+  templateUrl: './profiling.component.html',
+  styleUrl: './profiling.component.css'
+})
+export class ProfilingComponent {
+
+}
