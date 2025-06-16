9ff9c5e1fce183259be96bf69dca8441095eac35
NourJL1 <jlassinour.pro@gmail.com>
2025-06-16 16:20:42 +0100

    .

---
+++ src/app/entities/customer-doc-liste.ts
@@ -0,0 +1,14 @@
+
+import { CustomerDoc } from './customer-doc';
+
+export class CustomerDocListe {
+
+     cdlCode?: number;
+  cdlIden?: string;
+  cdlLabe?: string;
+  customerDocs?: CustomerDoc[];
+
+  constructor(init?: Partial<CustomerDocListe>) {
+    Object.assign(this, init);
+  }
+}
---
+++ src/app/entities/customer-identity.ts
@@ -0,0 +1,19 @@
+
+import { CustomerIdentityType } from './customer-identity-type';
+import { CustomerDocListe } from './customer-doc-liste';
+export class CustomerIdentity {
+    cidCode?: number;
+  cidNum?: string;
+  cidIden?: string;
+
+  customerIdentityType?: CustomerIdentityType;
+  customerDocListe?: CustomerDocListe;
+
+  constructor(init?: Partial<CustomerIdentity>) {
+    Object.assign(this, init);
+  }
+
+  isEmpty(): boolean {
+    return !this.cidNum || this.cidNum.trim().length === 0;
+  }
+}
