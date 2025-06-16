71a3bf13c49cfa08d17f86c158338eef4f241b19
JihenAridhi <aridhijihen1@gmail.com>
2025-06-16 16:27:18 +0100

    fees

--- src/app/components/security/admin-dashboard/accounting/accounting.component.ts
+++ src/app/components/security/admin-dashboard/accounting/accounting.component.ts
@@ -8,54 +8,51 @@ import { Component } from '@angular/core';
   styleUrl: './accounting.component.css'
 })
 export class AccountingComponent {
+  isFeeVisible: boolean = false;
+  isFeeSchemaVisible: boolean = false;
+  isFeeRuleVisible: boolean = false;
+  isFeeRuleTypeVisible: boolean = false;
+  isNewOperationTypeVisible: boolean = false;
+  isOperationMappingVisible: boolean = false;
 
-    isNewSchemaVisible: boolean = false;
-    isNewOperationTypeVisible: boolean = false;
-    isNewFeeVisible: boolean = false;
-    isNewMappingVisible: boolean = false;
-    isNewVatVisible: boolean = false;
-
-  toggleSchemaForm(): void {
-    this.isNewSchemaVisible = true;
-  }
-  closeSchemaForm(): void {
-    this.isNewSchemaVisible = false;
-  }
-
-   toggleForm(modal: String): void {
-    switch (modal)
-    {
-      case 'schema': this.isNewSchemaVisible = true; break;
-      case 'operation': this.isNewOperationTypeVisible = true; break;
-      case 'fee': this.isNewFeeVisible = true; break;
-      case 'mapping': this.isNewMappingVisible = true; break;
-      case 'vat' : this.isNewVatVisible = true; break;
+  toggleForm(modal: String): void {
+    switch (modal) {
+      case 'fee': this.isFeeVisible = true; break;
+      case 'fee-schema': this.isFeeSchemaVisible = true; break;
+      case 'fee-rule': this.isFeeRuleVisible = true; break;
+      case 'fee-rule-type': this.isFeeRuleTypeVisible = true; break;
+      case 'operation-type': this.isNewOperationTypeVisible = true; break;
+      case 'operation-mapping': this.isOperationMappingVisible = true; break;
     }
   }
   closeForm(modal: String): void {
-    switch (modal)
-    {
-      case 'schema': this.isNewSchemaVisible = false; break;
-      case 'operation': this.isNewOperationTypeVisible = false; break;
-      case 'fee': this.isNewFeeVisible = false; break;
-      case 'mapping': this.isNewMappingVisible = false; break;
-      case 'vat' : this.isNewVatVisible = false; break;
+    switch (modal) {
+      case 'fee': this.isFeeVisible = false; break;
+      case 'fee-schema': this.isFeeSchemaVisible = false; break;
+      case 'fee-rule': this.isFeeRuleVisible = false; break;
+      case 'fee-rule-type': this.isFeeRuleTypeVisible = false; break;
+      case 'operation-type': this.isNewOperationTypeVisible = false; break;
+      case 'operation-mapping': this.isOperationMappingVisible = false; break;
     }
   }
 
   get isAnyModalVisible(): boolean {
     return (
-      this.isNewSchemaVisible ||
+      this.isFeeVisible ||
+      this.isFeeSchemaVisible ||
       this.isNewOperationTypeVisible ||
-      this.isNewFeeVisible ||
-      this.isNewMappingVisible ||
-      this.isNewVatVisible
+      this.isFeeRuleVisible ||
+      this.isFeeRuleTypeVisible ||
+      this.isOperationMappingVisible
     );
   }
 
-  showTab(tabId: string): void {
-    const tabButtons = document.querySelectorAll('.tab-button');
-    const tabContents = document.querySelectorAll('.tab-content');
+  showTab(tabId: string, tabType?: string): void {
+
+    const buttonClass = tabType ? `${tabType}-tab-button` : 'tab-button';
+    const contentClass = tabType ? `${tabType}-tab-content` : 'tab-content';
+    const tabButtons = document.querySelectorAll(`.${buttonClass}`);
+    const tabContents = document.querySelectorAll(`.${contentClass}`);
 
     // Reset all buttons and contents
     tabButtons.forEach(btn => {
@@ -66,11 +63,12 @@ export class AccountingComponent {
     tabContents.forEach(content => content.classList.add('hidden'));
 
     // Activate the clicked button and show its tab content
-    const activeButton = document.getElementById(tabId);
+    const activeButton = tabType ? document.getElementById(tabType + '-' + tabId) : document.getElementById(tabId);
     activeButton?.classList.add('active', 'text-primary', 'font-medium');
     activeButton?.classList.remove('text-gray-500');
 
-    const activeContent = document.getElementById(`tab-${tabId}`);
+    const activeId = tabType ? `${tabType}-tab-${tabId}` : `tab-${tabId}`;
+    const activeContent = document.getElementById(activeId);
     activeContent?.classList.remove('hidden');
   }
 
